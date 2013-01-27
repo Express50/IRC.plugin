ed4c3d3986f5e2a0deade334e74394d6322111d3
Kevin Brown <bass5098@gmail.com>
2013-01-27 18:04:49 -0500

    Initial project setup

---
+++ IRC.plugin/IRC.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using EECloud.API;
+
+namespace IRC.plugin
+{
+    [Plugin(Authors = new string[] { "Bass5098", "Express50" },
+           Category = PluginCategory.Tool,
+           ChatName = "Editor",
+           Version = "0.0.1")]
+    public class IRC : Plugin<Player, IRC>
+    {
+
+    }
+}
