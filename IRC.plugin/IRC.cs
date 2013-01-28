d51c081d4101b2d80e5af2c79e765ec9b667be96
Express50 <press.ex50@gmail.com>
2013-01-28 11:24:50 -0500

    Added Connect/Disconnect and SendData functions

--- IRC.plugin/IRC.cs
+++ IRC.plugin/IRC.cs
@@ -2,23 +2,75 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Net.Sockets;
+using System.IO;
 using EECloud.API;
 
 namespace IRC.plugin
 {
     [Plugin(Authors = new string[] { "Bass5098", "Express50" },
            Category = PluginCategory.Tool,
-           ChatName = "Editor",
+           ChatName = "IRC",
            Version = "0.0.1")]
     public class IRC : Plugin<Player, IRC>
     {
-        private string server = "irc.rizon.net";
-        int port = 6667;
-        string nick = "RunBot";
-        string channel = "#RunEE";
+        private static string server = "irc.rizon.net";
+        private static int port = 6667;
+        private static string nick = "RunBot";
+        private static string channel = "#RunEE";
 
-        NetworkStream ns = null;
+        static NetworkStream nstream = null;
+        static StreamWriter writer = null;
+        static StreamReader reader = null;
 
-        TcpClient irc = null;
+        private static TcpClient client = null;
+
+        public static void Connect()
+        {
+            try
+            {
+                client = new TcpClient(server, port);
+                nstream = client.GetStream();
+
+                reader = new StreamReader(nstream);
+                writer = new StreamWriter(nstream);
+            }
+
+            catch
+            {
+
+            }
+        }
+
+        private static void Disconnect()
+        {
+            SendData("QUIT");
+
+            if (reader != null)
+                reader.Close();
+
+            if (writer != null)
+                writer.Close();
+
+            if (nstream != null)
+                nstream.Close();
+
+            if (client != null)
+                client.Close();
+        }
+
+        private static void SendData(string cmd, string param = null)
+        {
+            if (param == null)
+            {
+                writer.WriteLine(cmd);
+                writer.Flush();
+            }
+
+            else
+            {
+                writer.WriteLine(cmd + " " + param);
+                writer.Flush();
+            }
+        }
     }
 }
