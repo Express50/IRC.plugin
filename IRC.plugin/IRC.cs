b731ad1f43b7b6250261f586dff578f6618d4667
Express50 <press.ex50@gmail.com>
2013-01-28 14:35:45 -0500

    Fixed PluginCategory

--- IRC.plugin/IRC.cs
+++ IRC.plugin/IRC.cs
@@ -8,7 +8,7 @@ using EECloud.API;
 namespace IRC.plugin
 {
     [Plugin(Authors = new string[] { "Bass5098", "Express50" },
-           Category = PluginCategory.Tool,
+           Category = PluginCategory.Admin,
            ChatName = "IRC",
            Description = "IRC interface for executing commands in your EECloud plugin.",
            Version = "0.0.1")]
