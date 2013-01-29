51b4f59756e9006dcba093f96bc838acac57bedf
Express50 <press.ex50@gmail.com>
2013-01-29 09:10:56 -0500

    Added helper classes, started message handling

--- IRC.plugin/IRC.cs
+++ IRC.plugin/IRC.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Net.Sockets;
 using System.IO;
 using EECloud.API;
+using IRC.plugin.Utils;
 
 namespace IRC.plugin
 {
@@ -14,10 +15,10 @@ namespace IRC.plugin
            Version = "0.0.1")]
     public class IRC : Plugin<Player, IRC>
     {
-        private static string server = "irc.rizon.net";
-        private static int port = 6667;
-        private static string nick = "RunBot";
-        private static string channel = "#RunEE";
+        private static string server;
+        private static int port;
+        private static string nick;
+        private static string channel;
 
         static NetworkStream nstream = null;
         static StreamWriter writer = null;
@@ -26,6 +27,14 @@ namespace IRC.plugin
         private static TcpClient client = null;
 
         private static bool isConnected = false;
+
+        public IRC(string Nick, string Channel, int Port = 6667, string Server = "irc.rizon.net")
+        {
+            server = Server;
+            port = Port;
+            nick = Nick;
+            channel = Channel;
+        }
 
         #region EECloud
         protected override void OnConnect()
@@ -119,11 +128,44 @@ namespace IRC.plugin
         private static void ParseMessage(string data)
         {
             string[] message = data.Split(':');
+            User sender = new User();
 
             if (message[0] == "PING")
             {
                 SendData("PONG", message[1]);
             }
+
+            else if (message[0].StartsWith(":"))
+            {
+                sender.Nick = message[0].Substring(message[0].IndexOf(':') + 1, message[0].IndexOf('!'));
+                sender.Realname = message[0].Substring(message[0].IndexOf('!') + 1, message[0].IndexOf('@'));
+                sender.Hostname = message[0].Substring(message[0].IndexOf('@') + 1);
+
+                switch (message[1])
+                {
+                    case "PRIVMSG":
+                        if (message[2] == channel && message[3].StartsWith("!"))
+                        {
+                            ExecuteCommand(message[3].Substring(message[3].IndexOf(':') + 1);
+                        }
+                        break;
+                    case "NOTICE":
+                        break;
+                    case "MODE":
+                        break;
+                    case "KICK":
+                        break;
+                    case "JOIN":
+                        break;
+                    case "PART":
+                        break;
+                }
+            }
+        }
+
+        private static void ExecuteCommand(string command)
+        {
+
         }
 
         #endregion
---
+++ IRC.plugin/Utils/User.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IRC.plugin.Utils
+{
+    public class User
+    {
+        private string nick;
+
+        public string Nick
+        {
+            get
+            {
+                return this.nick;
+            }
+
+            set
+            {
+                this.nick = Nick;
+            }
+        }
+
+        private string hostname;
+
+        public string Hostname
+        {
+            get
+            {
+                return this.hostname;
+            }
+
+            set
+            {
+                this.hostname = Hostname;
+            }
+        }
+
+        private string realname;
+
+        public string Realname
+        {
+            get
+            {
+                return this.realname;
+            }
+
+            set
+            {
+                this.realname = Realname;
+            }
+        }
+
+        private string modes;
+
+        public string Modes
+        {
+            get
+            {
+                return this.modes;
+            }
+
+            set
+            {
+                this.modes = Modes;
+            }
+        }
+    }
+}
