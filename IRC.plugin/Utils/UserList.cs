5126ed3891ef2a7d9c8319ba70c3c77fe1be505e
Kevin Brown <bass5098@gmail.com>
2013-02-01 20:43:04 -0500

    Created UserList to allow us to overload a regular list.

---
+++ IRC.plugin/Utils/UserList.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace IRC.plugin.Utils
+{
+    class UserList : List<User>
+    {
+        /// <summary>
+        /// Gets a user from the list by their nick.
+        /// </summary>
+        /// <param name="nick">The nick to search for.</param>
+        /// <returns>The User object if it finds one, null if it doesn't.</returns>
+        public User GetByNick(string nick)
+        {
+            return this.FirstOrDefault(((user) => user.Nick == nick));
+        }
+
+        /// <summary>
+        /// Gets a user from the list by their hostname.
+        /// </summary>
+        /// <param name="hostname">The hostname to search for.</param>
+        /// <returns>The User object if it finds one, null if it doesn't.</returns>
+        public User GetByHostname(string hostname)
+        {
+            return this.FirstOrDefault(((user) => user.Hostname == hostname));
+        }
+    }
+}
